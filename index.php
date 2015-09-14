9d9754cbd4e605bfd3ed8cf40e495f389e998b1b
Jonny Pearen <jonny_pearen@hotmail.com>
2015-09-13 21:16:04 -0700

    Added third student and commented code

--- index.php
+++ index.php
@@ -11,10 +11,13 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        //include the Student.php file to allow us to create Student objects
         include('Student.php');
         
+        //initialize an empty array that we will use to contain Student objects
         $students = array();
         
+        //create the first student
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -23,8 +26,11 @@ and open the template in the editor.
         $first->add_grade(65);
         $first->add_grade(75);
         $first->add_grade(55);
+        
+        //add the first student to the array
         $students['j123'] = $first;
         
+        //create the second student
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -34,8 +40,26 @@ and open the template in the editor.
         $second->add_grade(95);
         $second->add_grade(80);
         $second->add_grade(50);
+        
+        //add the second student to the array
         $students['a456'] = $second;
         
+        //create the third student
+        $third = new Student();
+        $third->surname = "Pearen";
+        $third->first_name = "Jonny";
+        $third->add_email('personal','jonnypearen@gmail.com');
+        $third->add_grade(90);
+        $third->add_grade(80);
+        $third->add_grade(70);
+        
+        //add the third student to the array
+        $students['p789'] = $third;
+        
+        //sort the students by key
+        ksort($students);	// one of the many sort functions
+        
+        //echo each student in the students array using the toString method
         foreach ($students as $student)
             echo $student->toString();
         ?>
